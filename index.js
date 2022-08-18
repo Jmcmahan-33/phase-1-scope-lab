a00a63136f1a5b57cab3b9de6c5b24ef6c514d56
Jared McMahan <jaredm788@gmail.com>
2022-08-18 10:37:02 -0600

    commit tree 4e83de4e1e5a49ddfec0dba55886f1fcc33e1738

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution in this file!
+var customerName = "bob"
+const leastFavoriteCustomer = "rick"
+
+function nameGlobal() {
+    return customerName
+
+
+}
+
+// first try
+// function upperCaseCustomerName() {
+//     // let upperCaseCustomerName = customerName
+//     if (upperCaseCustomerName = customerName) {
+//         return upperCaseCustomerName.toUpperCase
+//     }
+// }
+
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer() {
+     bestCustomer = "not bob"
+     console.log(customerName)
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob"
+}
+
+ function changeLeastFavoriteCustomer() {
+    // const leastFavoriteCustomer = "rick"
+    return leastFavoriteCustomer = "rick"
+ }
